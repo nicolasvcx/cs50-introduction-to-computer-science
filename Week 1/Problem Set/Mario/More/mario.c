1c6a96a29bc9fdd5ee586abceb065d59acfcb33a
nicolasvcx <103842944+nicolasvcx@users.noreply.github.com>
2022-08-26 20:32:25 -0300

    Create mario.c

---
+++ Week 1/Problem Set/Mario/More/mario.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height = 10;
+
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height <= 0 || height > 8) ;
+    // First loop will iterate through the height of the pyramid
+    for (int i = 0; i < height; i++)
+    {
+        // Second loop will iterate through starting with the height - 1, decreasing it's value, until it's equal to 1
+        for (int j = height - i; j > 1; j--)
+        {
+            printf(" ");
+        }
+        for (int k = i; k >= 0; k--)
+        {
+            // Third loop will print the hashes, wich will increase every time the first loop finishes
+            printf("#");
+        }
+        printf("  "");
+        for (int k = i; k >= 0; k--)
+        {
+            printf("#");
+        }
+        // Will print a new line before the loops restart
+        printf("\n");
+    }
+}
